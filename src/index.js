1e4e38fbf01d079ce697954b21fcc887a4eaac32
Anderson Sanches <anderson@MacBook-de-Anderson.local>
2021-05-04 06:09:11 -0400

    Challenge concluded

--- src/index.js
+++ src/index.js
@@ -10,19 +10,89 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(
+    (user) => user.username === username
+  );
+
+  if (!user) {
+    return response.status(404).json({
+      error: "User not found!"
+    });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const countTodos = user.todos.length;
+
+  if (user.pro === false && countTodos > 9) {
+    return response.status(403).json({
+      error: "Maximum limit of 10 Todos for the free plan!"
+    });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(
+    (user) => user.username === username
+  );
+  
+  if (!user) {
+    return response.status(404).json({
+      error: "Username not found!"
+    });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: "Invalid uuid!"
+    });
+  }
+  
+  const todo = user.todos.find(
+    (todo) => todo.id === id
+  );
+
+  if (!todo) {
+    return response.status(404).json({
+      error: "Todo not found!"
+    });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(
+    (user) => user.id === id
+  );
+
+  if (!user) {
+    return response.status(404).json({
+      error: "User not found!"
+    });
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
